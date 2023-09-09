e8df698bf017a357fa40fe804d10c2283b3e961f
Mwjchapman <{23201760}+{mwjchapman}@users.noreply.github.com>
2023-09-09 16:41:40 +1200

    commiting JS intermediate lab work

---
+++ index.js
@@ -0,0 +1,11 @@
+/*const LA = "los angeles"
+
+const str = LA.split(" ");
+
+for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+}
+
+const la = str.join(" ");
+console.log(la);
+*/
